b5038f4ad086456bdfa11a2aa4c0eaf25ffb4f1f
lyperng <133280293+lyperng@users.noreply.github.com>
2023-05-20 20:19:58 -1000

    updated map

--- scripts/MHI map.R
+++ scripts/MHI map.R
@@ -1,7 +1,8 @@
 # make color coded MHI map
 
+##################################################
 # install and load libraries
-PKG <- c('tidyverse','ggspatial','mapdata', 'marmap', 'sf','rnaturalearth', 'rnaturalearthdata')
+PKG <- c('tidyverse','ggspatial','mapdata', 'marmap', 'sf','tigris','rnaturalearth','rnaturalearthdata')
 #c("ggplot2", 'ggmap', 'maps','usmap', 'ggsn', 'usmap', "RColorBrewer")
 # 'marmap' for visualizing marine data, including bathymetric contours
 # 'ggrepel' could be useful for making sure labels don't run into each other
@@ -14,60 +15,79 @@ for (p in PKG) {
     require(p,character.only = TRUE)}
 }
 
-worldmap<-map_data('world')
-HI <-subset(worldmap, subregion == 'Hawaii')
-HI$group<-factor(HI$group)
-
-# Transform data points into geographic objects
-HIsf <- HI %>%
-  st_as_sf(coords = c("long", "lat"), crs = 4326)
-
-HIpoly = st_sf(
-  aggregate(
-    HIsf$geometry,
-    list(HIsf$group),
-    function(g){
-      st_cast(st_combine(g),"POLYGON")
-    }
-  ))
-
-HIpoly$county<-c("Hawai'i",'Maui','Maui','Maui','Honolulu',"Kaua'i","Kaua'i")
-HIpoly$county <- factor(HIpoly$county, levels = c("Kaua'i", "Honolulu", 'Maui', "Hawai'i"))
-
+##################################################
 # Get bathymetry data
 bathy <- getNOAA.bathy(-160.5, -154.5, 18.5, 22.5, res = 1, keep = TRUE)
 ggbathy <- fortify(bathy)
 
 ##################################################
-# everything above and below this works together to create a nice map
-# in this block, I am essentially plotting the same map, but with the CCD outlines
+# get tiger shapefiles with the fishing community (CCD) outlines
 
-# read in census data from tiger/line shapefiles
-tracts <- st_read("data/tracts/tl_rd22_15_tract.shp")
-head(tracts)
-tracts_wgs<-st_transform(tracts,"+proj=longlat +datum=WGS84")
-head(tracts_wgs)
-
-ccd<-st_read("data/ccd/tl_2010_15_cousub10.shp")
+# read in census data from downloaded tiger/line shapefiles
+ccd<-st_read("data/ccd/tl_2010_15_cousub00.shp") # read in from download
 head(ccd)
 ccd_wgs<-st_transform(ccd,"+proj=longlat +datum=WGS84")
 head(ccd_wgs)
 
+# or get directly using  'tigris' package
+HIccd<-county_subdivisions('HI')
+head(HIccd)
+HIccd_wgs<-st_transform(HIccd,"+proj=longlat +datum=WGS84")
+head(HIccd_wgs)
+
+HIccd_land<-erase_water(HIccd_wgs) # erase the portion of ccd that is water 'tigris' pkg
+
+# read in water data
+counties<-unique(HIccd_land$COUNTYFP) # what are the diff county codes?
+water<-area_water('HI',counties) # get water area for all counties
+
 # assign county names for nicer plotting
-ccd_wgs <- ccd_wgs %>% mutate(County = case_when(
-  COUNTYFP10 == '001' ~ "Hawai'i",
-  COUNTYFP10 == '003' ~ "Honolulu",
-  COUNTYFP10 == '005' ~ NA,
-  COUNTYFP10 == '007' ~ "Kaua'i",
-  COUNTYFP10 == '009' ~ 'Maui'
+HIccd_land <- HIccd_land %>% mutate(County = case_when(
+  COUNTYFP == '001' ~ "Hawai'i",
+  COUNTYFP == '003' ~ "Honolulu",
+  COUNTYFP == '005' ~ NA,
+  COUNTYFP == '007' ~ "Kaua'i",
+  COUNTYFP == '009' ~ 'Maui'
 ))
+
+counordered <- c("Kaua'i", "Honolulu", "Maui", "Hawai'i")
+HIccd_land$County<-factor(HIccd_land$County, levels = counordered)
 
 ##################################################
 
-###### below is in progress of trying to plot the MHI and bathy data together
-# so far, the MHI data alone works fine using geom_polygon, but cant add the contours because they are on diff projections
+######## plot MHI map with fishing comms (CCDs) marked and bathy contours #######
 
-# plot map
+### first plot inset world map
+world <- ne_countries(scale='medium',returnclass = 'sf') # using rnaturalearthdata
+class(world)
+st_crs(world)
+  
+# Set the CRS to the Lambert azimuthal equal-area projection centered on 120 longitude
+crs <- "+proj=laea +lat_0=52 +lon_0=-1200000 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"
+world <- st_transform(world, crs)
+
+# Create a bounding box for Hawaii
+hawaii_bbox <- st_bbox(c(xmin = -162, xmax = -153, ymax = 18, ymin = 23), crs = st_crs(4326))
+hawaii_bbox <- st_as_sfc(hawaii_bbox)
+hawaii_bbox <- st_sf(geometry = hawaii_bbox)
+
+# Plot the world map
+inset<-ggplot() +
+  geom_sf(data = world) +
+  geom_sf(data = hawaii_bbox, fill = NA, color = "red", linewidth = 0.7) +
+  theme_bw() +
+  coord_sf(crs = st_crs(world), 
+           xlim = c(-3000000, 10000000), 
+           ylim = c(-2000000, 7000000),expand = FALSE) +
+  theme(axis.line = element_blank(),
+        axis.text = element_blank(),
+        axis.ticks = element_blank(),
+        axis.title = element_blank())
+inset
+
+######## plot MHI map ########
+
+### add contours
 map_cont<- ggplot() +
   theme_bw() +
   geom_contour(
@@ -88,8 +108,11 @@ map_cont<- ggplot() +
   
 map_cont
 
+### add hawaii ccd and water layers
 map1<-map_cont +
-   geom_sf(data = ccd_wgs, aes(fill = County)) + # plot the ccd data
+   geom_sf(data = HIccd_land, aes(fill = County)) + # plot the ccd data
+   geom_sf(data = water, fill = alpha('#A6CEE3',0.3)) + # plot the water data
+   geom_sf(data=HIccd_wgs, fill=alpha('grey',0)) + # add ccd outlines on the water too
 #   geom_sf(data = HIpoly, aes(group = Group.1, fill = county), color = "black") + # plot simple island outlines   
   coord_sf(xlim = c(-160.5, -154.6), ylim = c(18.7, 22.4), expand = FALSE) +
   # set the map edge limits, expand F means nothing can go past these limits
@@ -100,13 +123,14 @@ map1<-map_cont +
        x='Longitude', y='Latitude')
 map1
 
+### adjust layout and aesthetics
 map2<-map1 +
   theme(plot.title = element_text(hjust = 0.5, size = 28),              
         axis.title=element_text(size=22,face="plain"), #adjust size of axis titles
         axis.text=element_text(size=16, color = 'black'), #adjust font size of axis tick labels
-        legend.position = c(0.16, 0.27),  # manually adjust legend position
+        legend.position = c(0.17, 0.48),  # manually adjust legend position
         legend.background = element_blank(), # need to set this otherwise it is opaque
-        legend.box.background = element_rect(color = 'black', fill=alpha("white", 0.5)),
+        legend.box.background = element_rect(color = 'black', fill=alpha("white", 0.4)),
         # set legend border, fill, transparency
         legend.key = element_rect(fill = "transparent", colour = NA),# if not set, 
         legend.key.size = unit(0.8, 'cm'), # adjust size of ea fill box
@@ -117,15 +141,15 @@ map2<-map1 +
   guides(fill=guide_legend(byrow = T))
 map2
 
+### add scalebar and north arrow
 map_scale <-map2 +
-  # Add scale and North arrow
   ggspatial::annotation_scale(
     style = 'ticks',
     height = unit(-0.25, "cm"),
     tick_height = 0.6,
     line_width = 1,
     location = "tr",
-    pad_x = unit(0.46, "in"), pad_y = unit(0.38, "in"),
+    pad_x = unit(0.56, "in"), pad_y = unit(0.38, "in"),
     width_hint = 0.17,
     text_pad = unit(-1.82, "in"),
     line_col = "grey30"
@@ -137,18 +161,25 @@ map_scale <-map2 +
     tick_height = 0.6,
     line_width = 1,
     location = "tr",
-    pad_x = unit(0.51, "in"), pad_y = unit(0.15, "in"),
+    pad_x = unit(0.61, "in"), pad_y = unit(0.15, "in"),
     width_hint = 0.17,
     text_pad = unit(-1.82, "in"),
     line_col = "grey30"
   ) +
   ggspatial::annotation_north_arrow(
     location = "tr", which_north = "true",
-    pad_x = unit(0.87, "in"), pad_y = unit(0.45, "in"),
+    pad_x = unit(0.96, "in"), pad_y = unit(0.45, "in"),
     style = ggspatial::north_arrow_nautical(
       fill = c("grey40", "white"),
       line_col = "grey20"
     )
+  )  +
+  annotation_custom(
+    grob = ggplotGrob(inset),
+    xmin = -160.3,
+    xmax = -158.7,
+    ymin = 18.85,
+    ymax = 19.85
   )
 
 map_scale
@@ -193,54 +224,3 @@ map<-
   labs(title = "Hawaii Islands", fill = "County") +
   theme(plot.title = element_text(hjust = 0.5, size = 18)) 
 map
-
-
-######################################
-# test script found online with bathymetric contours
-pays <- rnaturalearth::ne_countries(
-  country = c("France", "Spain"),
-  scale = "large", returnclass = "sf"
-)
-# Get bathymetry data
-bathy <- getNOAA.bathy(-8, 0, 42, 50, res = 1, keep = TRUE)
-ggbathy <- fortify(bathy)
-
-# Base plot
-pl <- ggplot(data = pays) +
-  geom_contour(
-    data = ggbathy, aes(x = x, y = y, z = z),
-    binwidth = 200, color = "grey80", size = 0.3
-  ) +
-  
-  geom_sf()+
-  coord_sf(xlim = c(-6, 0), ylim = c(43, 48.5), expand = FALSE) +
-  theme_bw()
-
-pl
-# Add scale and North arrow
-pl +
-  ggspatial::annotation_scale(
-    location = "tr",
-    bar_cols = c("grey60", "white"),
-    text_family = "ArcherPro Book"
-  ) +
-  ggspatial::annotation_north_arrow(
-    location = "tr", which_north = "true",
-    pad_x = unit(0.4, "in"), pad_y = unit(0.4, "in"),
-    style = ggspatial::north_arrow_nautical(
-      fill = c("grey40", "white"),
-      line_col = "grey20",
-      text_family = "ArcherPro Book"
-    )
-  )
-
-# started to try for MHI, but cannot filter to state data, only country works so far
-# using original code with data from usmap for now
-us <- ne_countries(country = 'United States of America',scale = "medium", returnclass = "sf")
-
-# Subset the data for Hawaii
-hawaii <- us %>%
-  filter(name == "Hawaii")
-
-# Plot the map of Hawaii
-plot(hawaii)
